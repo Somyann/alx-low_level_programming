4e41815987f9e53a0d557574304b20109bde9b90
Somyann <somiaprecious91@gmail.com>
2024-08-08 07:40:29 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
 * main - entry point
 *
@@ -8,6 +9,6 @@
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
